7aa71160da085db914acf94b382fd8f7c8f6de57
RichFisher <rich.fishernc@gmail.com>
2019-03-05 13:28:46 -0600

    legos my eggos

---
+++ legoclasses/MiniFigure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace legoclasses
+{
+    class MiniFigure
+    {
+        public string HairOrHat { get; set; }
+        public string Head { get; set; }
+        public string Torso { get; set; }
+        public string Legs { get; set; }
+        public string Accessories { get; set; }
+
+        public void Jump()
+        {
+            Console.WriteLine("The lego jumped");
+        }
+        public void DoubleJump()
+        {
+            Console.WriteLine("The lego double jumped");
+        }
+        public void Attack()
+        {
+            Console.WriteLine("The lego attacked");
+        }
+        public void SpecialAttack()
+        {
+            Console.WriteLine("The lego special attacked");
+        }
+        public void Look()
+        {
+            Console.WriteLine("The lego looked");
+        }
+        public void Move()
+        {
+            Console.WriteLine("The lego moved");
+        }
+    }
+}
--- legoclasses/Program.cs
+++ legoclasses/Program.cs
@@ -6,7 +6,28 @@ namespace legoclasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var Lego1 = new MiniFigure
+            {
+                HairOrHat = "Bald",
+                Head = "Pirate",
+                Torso = "Pirate",
+                Legs = "Pirate",
+                Accessories = "Parrot",
+            };
+            var Lego2 = new MiniFigure
+            {
+                HairOrHat = "Afro",
+                Head = "Cowboy",
+                Torso = "Cowboy",
+                Legs = "Cowboy",
+                Accessories = "Pistol",
+            };
+            Lego1.Move();
+            Lego1.Attack();
+            Lego1.Jump();
+            Lego2.SpecialAttack();
+            Lego2.DoubleJump();
+            Console.ReadLine();
         }
     }
 }
